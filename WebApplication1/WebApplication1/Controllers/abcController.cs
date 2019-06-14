1a1650a20f98c01597440ebe30c4feb27beb8dfa
M Areeb Siddiqui <16b-043-bs@uit.edu>
2019-06-14 15:50:11 +0500

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/abcController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class abcController : Controller
+    {
+        //
+        // GET: /abc/
+        public ActionResult Index()
+        {
+            Class1 c  = new Class1();
+            c.employeeId = 1234;
+            return View("Index",c);
+        }
+	}
+}
---
+++ WebApplication1/WebApplication1/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Class1
+    {
+        public int employeeId { get; set; }
+
+    }
+}
